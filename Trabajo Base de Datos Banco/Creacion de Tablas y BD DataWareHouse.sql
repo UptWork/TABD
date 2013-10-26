7583353eb8a0b5a2da5adb648f150d9e92b9d1f0
UptWork <uptwork@gmail.com>
2013-10-26 10:50:31 -0500

    Update Creacion de Tablas y BD DataWareHouse.sql

--- Trabajo Base de Datos Banco/Creacion de Tablas y BD DataWareHouse.sql
+++ Trabajo Base de Datos Banco/Creacion de Tablas y BD DataWareHouse.sql
@@ -14,7 +14,7 @@ CREATE TABLE DimCuenta (
 	fechaInicio datetime not null,
 	fechaFin datetime not null,
 	indicadorActivo bit,
-	constraint PK_DimCuenta primary key (CuentaSK),
+	constraint PK_DimCuenta primary key (CuentaSK)
 )
 GO
 CREATE TABLE DimEmpleado (
@@ -29,7 +29,7 @@ CREATE TABLE DimEmpleado (
 	fechaInicio datetime not null,
 	fechaFin datetime not null,
 	indicadorActivo bit,
-	constraint PK_DimEmpleado primary key (EmpleadoSK),
+	constraint PK_DimEmpleado primary key (EmpleadoSK)
 )
 GO
 CREATE TABLE DimTiempo(
@@ -43,7 +43,7 @@ CREATE TABLE DimTiempo(
 )
 GO
 CREATE TABLE DimSucursal(
-	sucursalSK BIGINT not null,
+	sucursalSK BIGINT identity not null,
 	idSucursal BIGINT not null,
 	nombreSucursal varchar(50) not null,
 	Direccion varchar(100) not null,
@@ -55,7 +55,7 @@ CREATE TABLE DimSucursal(
 GO
 
 CREATE TABLE FactMovimiento(
-	idMovimiento BIGINT identity not null,
+	idMovimiento BIGINT not null,
 	cantidadMovimiento decimal(8,2) not null,
 	tiempoSK DATETIME not null,
 	sucursalSK BIGINT not null,
@@ -66,5 +66,48 @@ CREATE TABLE FactMovimiento(
 	constraint FK_DimSucursal_FactMovimiento foreign key(sucursalSK) references DimSucursal(SucursalSK),
 	constraint FK_DimTiempo_FactMovimiento foreign key(tiempoSK) references DimTiempo(TiempoSK),
 	constraint FK_DimEmpleado_FactMovimiento foreign key(empleadoSK) references DimEmpleado(EmpleadoSK)
+)
+GO
+
+
+--Tablas temporales
+
+CREATE TABLE TMPDimCuenta (
+	idCuenta BIGINT not null,
+	primerNombre varchar(30) not null,
+	segundoNombre varchar(30) null,
+	apellidoPaterno varchar(30) not null,
+	apellidoMaterno varchar(30) not null,
+	fechaNacimiento date not null,
+	sexo varchar(1) not null
+)
+GO
+
+CREATE TABLE TMPDimEmpleado (
+	idEmpleado BIGINT not null,
+	primerNombre varchar(30) not null,
+	segundoNombre varchar(30) null,
+	apellidoPaterno varchar(30) not null,
+	apellidoMaterno varchar(30) not null,
+	fechaNacimiento date not null,
+	sexo varchar(1) not null
+)
+
+GO
+
+CREATE TABLE TMPDimSucursal(
+	idSucursal BIGINT not null,
+	nombreSucursal varchar(50) not null,
+	Direccion varchar(100) not null
+)
+GO
+
+CREATE TABLE TMPFactMovimiento(
+	idMovimiento BIGINT not null,
+	cantidadMovimiento decimal(8,2) not null,
+	tiempoSK DATETIME not null,
+	sucursalSK BIGINT not null,
+	cuentaSK BIGINT not null,
+	empleadoSK bigint not null
 )
 GO
